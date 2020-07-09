959c5f77b9b614b31d33db855d10e8d3794ac88b
Hannes Preishuber <hannesp@ppedv.de>
2020-07-09 17:05:13 +0200

    Todo Edit und localstorage

---
+++ BlazorTraining3.2/Data/Aufgaben.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc.Diagnostics;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorTraining3._2.Data
+{
+    public class Aufgaben
+    {
+        public int ID { get; set; }
+        public string Text { get; set; }
+        public DateTime Datum { get; set; }
+        public Boolean Erledigt { get; set; }
+        [NotMapped]
+
+        public Boolean IsEdit { get; set; }
+    }
+}
