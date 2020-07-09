7ee51dab91ace86333919fbc34572fe9e3849318
Hannes Preishuber <hannesp@ppedv.de>
2020-07-09 12:12:57 +0200

    Razor Class Library

---
+++ BlazorTraining3.2/Pages/Hannes.razor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorTraining3._2.Pages
+{
+    public partial class Hannes
+    {
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+        }
+    }
+}
