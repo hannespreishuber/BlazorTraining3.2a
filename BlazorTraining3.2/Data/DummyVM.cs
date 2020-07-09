6424928d5c6ad15fcc67be27617e7e58610dbed9
Hannes Preishuber <hannesp@ppedv.de>
2020-07-09 15:41:20 +0200

    Viewmodel Dummy property, Method, und Event

---
+++ BlazorTraining3.2/Data/DummyVM.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorTraining3._2.Data
+{
+    public class DummyVM
+    {
+        public int MyProperty { get; set; }
+        public void Add()
+        {
+            MyProperty++;
+            MyEvent.Invoke();
+           
+        }
+
+      public event Action MyEvent;
+
+    }
+}
