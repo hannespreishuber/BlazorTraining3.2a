5887c1ec3c202272834c148311f2cea39aedbef2
Hannes Preishuber <hannesp@ppedv.de>
2020-07-09 13:56:51 +0200

    Blazor Demos

--- BlazorTraining3.2/Pages/Hannes.razor.cs
+++ BlazorTraining3.2/Pages/Hannes.razor.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BlazorTraining3._2.Data;
+using Microsoft.AspNetCore.Components;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,9 +9,11 @@ namespace BlazorTraining3._2.Pages
 {
     public partial class Hannes
     {
+        [Inject] WeatherForecastService ForecastService{ get; set; }
+  
         protected override void OnInitialized()
         {
-            base.OnInitialized();
+           
         }
     }
 }
