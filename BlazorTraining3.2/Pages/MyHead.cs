744e31a8c5ea010b1d0278231d81c803b87aecc5
Hannes Preishuber <hannesp@ppedv.de>
2020-07-10 13:59:13 +0200

    Component Rendertree

---
+++ BlazorTraining3.2/Pages/MyHead.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorTraining3._2.Pages
+{
+    public class MyHead : ComponentBase
+
+    {
+        [Parameter]
+        public string Title { get; set; }
+        protected override void BuildRenderTree(RenderTreeBuilder builder)
+        {
+            builder.OpenElement(1, "H1");
+            builder.AddContent(2,Title);
+            builder.CloseElement();
+
+        }
+    }
+}
